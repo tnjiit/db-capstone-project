50da3e3eb60ba06c6b56bae9f22e74db4e8bb344
Tushar Jain <tusharjain@fb.com>
2023-09-05 10:59:46 -0700

    Removing redundant files.

--- PreparedStatement.sql
+++
@@ -1,9 +0,0 @@
--- Active: 1693790973428@@127.0.0.1@3306@LittleLemonDB
-USE LittleLemonDB;
-
-PREPARE GetOrderDetail FROM
-'SELECT * FROM Orders WHERE OrderID=?;'
-
-
-SET @var := 1;
-EXECUTE GetOrderDetail USING @var;
